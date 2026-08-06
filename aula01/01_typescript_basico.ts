2cae5f7078129544b774349bf0ef6b562e9d9359
Max <camilamachadodesouza77@gmail.com>
2026-08-05 21:45:13 -0300

    @MaxPichy

---
+++ aula01/01_typescript_basico.ts
@@ -0,0 +1,10 @@
+// 1. Tipagem Implícita
+let nomeEstudante = 'Gustavo';
+// nomeEstudante = 420;
+
+// 2. Tipagem Explícita
+let matriculado: boolean = true;
+let idadeAluno: number = 20;
+let habilidades: string[] = ['HTML', 'CSS', 'JavaScript'];
+let habilidades2: Array<string> = ['HTML', 'CSS', 'JavaScript','TypeScript'];
+let notas: Array<number> = [8.5, 9.0, 7.5];
---
+++ aula01/app-revisao/src/App.jsx
@@ -0,0 +1,5 @@
+import { ContadorPlayground } from "./ContadorPlayground";
+
+export default function App(){
+  return <ContadorPlayground titulo='Contador Revisão' valorInicial={10} />
+}
