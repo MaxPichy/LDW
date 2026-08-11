4a8ef25064d99c01d97774f08215650abd0f9005
Max <camilamachadodesouza77@gmail.com>
2026-08-10 22:21:08 -0300

    @MaxPichy

--- projeto-monorepo/backend/src/server.ts
+++ projeto-monorepo/backend/src/server.ts
@@ -1,6 +1,7 @@
 import express, {Request, Response} from "express";
 import cors from 'cors';
 import dotenv from 'dotenv';
+import { sequelize } from './config/database';
 
 dotenv.config();
 
@@ -12,15 +13,26 @@ app.use(cors());
 app.use(express.json());
 
 // Rota de Health Check
-app.get('./api/health', (req: Request, res: Response) => {
+app.get('/api/health', (req: Request, res: Response) => {
     res.status(200).json({ 
         message: 'Servidor Backend rodando com sucesso.',
         timestamp: new Date().toISOString()
     });
 });
 
-app.listen(PORT, () => {
-    console.log(`Servidor rodando na porta ${PORT}`);
-    console.log(`Health Check disponível em: http://localhost:${PORT}/api/health`);
-});
+async function main() {
+    try{
+        await sequelize.authenticate();
+        console.log('Conexão com o PostgreSQL no Supabase realizada com sucesso.');
 
+        app.listen(PORT, () => {
+        console.log(`Servidor rodando na porta ${PORT}`);
+        console.log(`Health Check disponível em: http://localhost:${PORT}/api/health`);
+        });
+
+    }catch(error){
+        console.log('Erro ao conectar com o banco de dados.', error);
+    }
+}
+
+main();
