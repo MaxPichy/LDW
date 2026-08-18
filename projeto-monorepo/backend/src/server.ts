32de2b4dc500e5a43d218fde5abfb403a0619fac
Max <camilamachadodesouza77@gmail.com>
2026-08-17 22:55:19 -0300

    @MaxPichy

--- projeto-monorepo/backend/src/server.ts
+++ projeto-monorepo/backend/src/server.ts
@@ -4,6 +4,7 @@ import dotenv from 'dotenv';
 import { sequelize } from './config/database';
 import { User } from "./models/User";
 import { sensitiveHeaders } from "node:http2";
+import { appRoutes } from "./routes";
 
 dotenv.config();
 
@@ -23,35 +24,8 @@ app.get('/api/health', (req: Request, res: Response) => {
     });
 });
 
-// Cadastrar novo usuário
-app.post('/api/users', async (req: Request, res: Response) => {
-    try{
-        const {nome, email, senha_hash} = req.body;
-        if(!nome || !email || !senha_hash){
-            return res.status(400).json({erro: 'Nome, Email e Senha_Hash são obrigatórios.'});
-        }
-
-        const novoUsuario = await User.create({nome, email, senha_hash});
-
-        return res.status(201);
-
-    } catch(error: any){
-        return res.status(500).json({erro: 'Erro ao cadastrar usuário.', datalhe: error.message})
-    }
-});
-
-// Listar todos os usuários
-app.get('/api/users', async (req: Request, res: Response) => {
-    try{
-        const usuarios = await User.findAll({
-            attributes: ['id', 'nome', 'email', 'createdAt']
-        });
-        return res.status(200).json(usuarios);
-
-    } catch(error: any){
-        return res.status(500).json({erro: 'Erro ao listar usuários.', detalhe: error.message});
-    }
-});
+// Registra todas as rotas da aplicação sob o prefixo /api 
+app.use('/api', appRoutes);
 
 async function main() {
     try{
