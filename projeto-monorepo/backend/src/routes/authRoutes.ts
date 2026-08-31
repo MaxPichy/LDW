e64cc11bc7d6e76f3351ab8f9d218eb84cc832e8
Max <camilamachadodesouza77@gmail.com>
2026-08-31 20:41:57 -0300

    wip: auth feat and ci corrections

---
+++ projeto-monorepo/backend/src/routes/authRoutes.ts
@@ -0,0 +1,8 @@
+import { Router } from "express";
+import { AuthController } from "../controllers/AuthController";
+
+const router = Router();
+
+// Rota pública de login
+router.post('/login', AuthController.login);
+export {router as authRoutes}
